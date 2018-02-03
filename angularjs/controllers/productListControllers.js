c6710305ea6036df815d219167f02c20018a620c
Siwoo-Kim <skim327@myseneca.ca>
2018-02-03 14:19:35 -0500

    Angular_JS Learning version 1.0

---
+++ angularjs/controllers/productListControllers.js
@@ -0,0 +1,35 @@
+
+angular.module('sportsStore')
+    .constant('productLisgPageCount',3)
+    .constant('productListActiveClass','btn-primary')
+    .constant('productListClass','btn-secondary')
+    .controller('productListCtrl',function (
+        $scope, $filter
+        ,productListActiveClass,productListClass,
+        productLisgPageCount) {
+        var selectedCategory = null;
+
+        $scope.selectedPage = 1;
+        $scope.pageSize = productLisgPageCount;
+
+        $scope.selectCategory = function (newCategory) {
+            selectedCategory = newCategory;
+            $scope.selectedPage = 1;
+        }
+
+        $scope.selectPage = function (newPage) {
+            $scope.selectedPage = newPage;
+        }
+
+        $scope.getPageClass = function (page) {
+            return $scope.selectedPage == page ? productListActiveClass : productListClass;
+        }
+
+        $scope.categoryFilterFn = function (product) {
+            return selectedCategory == null || product.category == selectedCategory;
+        }
+
+        $scope.getCategoryClass = function (category) {
+            return selectedCategory == category ? productListActiveClass : productListClass;
+        }
+    })
